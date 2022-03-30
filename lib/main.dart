c1ba6a1e9dc93993f6f445e47e289e3fe74184c8
Michał Krysiński <mihalkrysinski8@gmail.com>
2022-03-30 20:00:51 +0200

    Add AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,29 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Notemobile'),
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {
+              print('Hello Notemobile');
+            },
+            icon: Icon(Icons.menu),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.notifications_none),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search),
+            ),
+          ],
+          backgroundColor: Color.fromARGB(255, 250, 167, 43),
+        ),
+      ),
     );
   }
 }
