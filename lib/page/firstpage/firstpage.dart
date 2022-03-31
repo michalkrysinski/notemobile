e907b857001f9d0799c54b6336c6e2fedba819b5
Michał Krysiński <mihalkrysinski8@gmail.com>
2022-03-31 04:09:03 +0200

    Add AnimatedTextKit

--- lib/page/firstpage/firstpage.dart
+++ lib/page/firstpage/firstpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class FirstPage extends StatefulWidget {
   const FirstPage({
@@ -13,32 +14,13 @@ class _FirstPage extends State<FirstPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('FirstPage'),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {
-            const FirstPage();
-          },
-          icon: const Icon(Icons.menu),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search),
-          ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text('Notemobile'),
         ],
-        elevation: 10,
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.black, Colors.yellow],
-              begin: Alignment.bottomRight,
-              end: Alignment.topLeft,
-            ),
-          ),
-        ),
-      ),
+      )),
     );
   }
 }
